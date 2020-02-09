06032e8443e152287481df65992fd598b03094d4
jessychen1016 <chenzexi1016@126.com>
2020-02-09 16:48:37 +0800

    finished generating the contrasted pictures

--- tuning_contrast/tune_contrast.py
+++ tuning_contrast/tune_contrast.py
@@ -3,39 +3,63 @@ from builtins import input
 import cv2 as cv
 import numpy as np
 import argparse
+import glob
+import os
+
 # Read image given by user
-parser = argparse.ArgumentParser(description='Code for Changing the contrast and brightness of an image! tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='./195.jpg')
+parser = argparse.ArgumentParser(description='Code for Changing the contrast and brightness of an image! ')
+parser.add_argument('--input', help='Path to input image.', default='./zjg_ground_image/')
+parser.add_argument('--output', help='Path to input image.', default='./contrast_image/')
 args = parser.parse_args()
-image = cv.imread(cv.samples.findFile(args.input))
-# if image is None:
-#     print('Could not open or find the image: ', args.input)
-#     exit(0)
-# new_image = np.zeros(image.shape, image.dtype)
-# alpha = 1.0 # Simple contrast control
-# beta = 0    # Simple brightness control
-# # Initialize values
-# print(' Basic Linear Transforms ')
-# print('-------------------------')
-# try:
-#     alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
-#     beta = int(input('* Enter the beta value [0-100]: '))
-# except ValueError:
-#     print('Error, not a number')
-# # # Do the operation new_image(i,j) = alpha*image(i,j) + beta
-# # # Instead of these 'for' loops we could have used simply:
-# # # new_image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
-# # # but we wanted to show you how to access the pixels :)
-# # for y in range(image.shape[0]):
-# #     for x in range(image.shape[1]):
-# #         for c in range(image.shape[2]):
-# #             new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
-# new_image=cv.convertScaleAbs(image, alpha=alpha, beta=beta)
-cv.imshow('Original Image', image)
-cv.waitKey(1000)
-imghsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+inputdir = args.input
+outputdir = args.output
+
+for filename in glob.glob(inputdir+"*.jpg"):
+
+	image = cv.imread(filename)
+	# if image is None:
+	#     print('Could not open or find the image: ', args.input)
+	#     exit(0)
+	# new_image = np.zeros(image.shape, image.dtype)
+	# alpha = 1.0 # Simple contrast control
+	# beta = 0    # Simple brightness control
+	# # Initialize values
+	# print(' Basic Linear Transforms ')
+	# print('-------------------------')
+	# try:
+	#     alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
+	#     beta = int(input('* Enter the beta value [0-100]: '))
+	# except ValueError:
+	#     print('Error, not a number')
+	# # # Do the operation new_image(i,j) = alpha*image(i,j) + beta
+	# # # Instead of these 'for' loops we could have used simply:
+	# # # new_image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+	# # # but we wanted to show you how to access the pixels :)
+	# # for y in range(image.shape[0]):
+	# #     for x in range(image.shape[1]):
+	# #         for c in range(image.shape[2]):
+	# #             new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+	# new_image=cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+	imghsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
 
-imghsv[:,:,2] = [[max(0.5*pixel, 0) if pixel < 60 else min(pixel+0.8*(255-pixel), 255) for pixel in row] for row in imghsv[:,:,2]]
-cv.imshow('contrast', cv.cvtColor(imghsv, cv.COLOR_HSV2BGR))
-cv.waitKey(20000)
+	# for row in imghsv[:,:,2]:
+	# 	for pixel in row:
+	# 		if pixel == 255:
+	# 			pixel = 255
+	# 		elif pixel < 60:
+	# 			pixel=max(0.2*pixel, 0)
+	# 		else:
+	# 			pixel = pixel + 0.5*(255-pixel)
+
+	imghsv[:,:,2] = [[max(0.3*pixel, 0) if pixel < 60 else min(pixel+0.6*(255-pixel), 255) for pixel in row] for row in imghsv[:,:,2]]
+	imgBGR = cv.cvtColor(imghsv, cv.COLOR_HSV2BGR)
+	imgGRAY = cv.cvtColor(imgBGR, cv.COLOR_BGR2GRAY)
+	picturename = os.path.basename(filename)
+	print("picturename", picturename )
+	cv.imwrite(outputdir+picturename, imgGRAY, [cv.IMWRITE_JPEG_QUALITY, 100])
+	print("Picture:", filename, "done")
+	# cv.imshow('contrast', imgGRAY)
+	# cv.waitKey(1000)
